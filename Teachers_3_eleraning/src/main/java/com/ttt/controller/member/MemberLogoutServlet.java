ddc9f02409a823bed414e939ff8b1f1ccc92ce71
hoonee-math <kwanghoon041@gmail.com>
2025-01-07 01:31:08 +0900

    gh/개선/로그아웃 요청 처리후 주소창의 주소가 /member/logout 인 문제를 해결하기 위해 msg.jsp 로 응답 ***

--- Teachers_3_eleraning/src/main/java/com/ttt/controller/member/MemberLogoutServlet.java
+++ Teachers_3_eleraning/src/main/java/com/ttt/controller/member/MemberLogoutServlet.java
@@ -20,7 +20,14 @@ public class MemberLogoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
 		session.invalidate();
-		request.getRequestDispatcher("/").forward(request, response);
+		
+		//요청 처리후 주소창의 주소가 /member/logout 인 문제를 해결하기 위해 msg.jsp 로 응답
+		String msg="로그아웃 되었습니다.";
+		String loc="/";
+		request.setAttribute("msg", msg);
+		request.setAttribute("loc", loc);
+		
+		request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
 		
 		
 		
