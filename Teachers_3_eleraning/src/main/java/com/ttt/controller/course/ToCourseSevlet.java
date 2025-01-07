e956742a3c8cc5a12c55ac7b74d0964c99953d48
이민영 <ffrf1234tina@gmail.com>
2025-01-07 11:49:51 +0900

    my/수정/강좌 리스트 불러오는 servlet, service, dao 작업 완, 점심 이후에 mapper 작업 해야함

--- Teachers_3_eleraning/src/main/java/com/ttt/controller/course/ToCourseSevlet.java
+++ Teachers_3_eleraning/src/main/java/com/ttt/controller/course/ToCourseSevlet.java
@@ -1,8 +1,6 @@
 package com.ttt.controller.course;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ttt.dto.Course3;
+import com.ttt.service.CourseService;
 
 @WebServlet("/course/list")
 public class ToCourseSevlet extends HttpServlet {
@@ -21,7 +22,9 @@ public class ToCourseSevlet extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		// 파라미터 처리
+		int subjectNo = Integer.parseInt(request.getParameter("subjectName"));
+		
 		// 페이징 처리를 위한 현재 페이지 정보
 		int cpage = 1;
 		try {
@@ -30,25 +33,12 @@ public class ToCourseSevlet extends HttpServlet {
 		}
 
 		int numPerPage = 2; // 한 페이지당 데이터 수
-
-		// 더미 데이터 생성
-		List<Map<String, Object>> courses = new ArrayList<>();
-
-		// 더미 데이터 10개 추가
-		for (int i = 1; i <= 10; i++) {
-			Map<String, Object> course = new HashMap<>();
-			course.put("courseNo", i);
-			course.put("courseTitle", "2024 수능대비 " + i + "번 강좌");
-			course.put("courseDesc", i + "번 강좌에 대한 상세 설명입니다.");
-			course.put("coursePrice", 150000 + (i * 10000));
-			course.put("coursePriceSale", 10); // 10% 할인
-			course.put("memberNo", i);
-			course.put("memberName", "강기동" + i);
-			course.put("teacherSubject", "국어");
-			course.put("teacherInfoTitle", "개념과 지독한 연습이 만점을 만듭니다!");
-			course.put("imageNo", i);
-			courses.add(course);
-		}
+		
+		Map<String, Integer> param = Map.of(
+					"cpage", cpage,
+					"numPerPage", numPerPage,
+					"subjectNo", subjectNo);
+		List<Course3> courses = new CourseService().selectCourseBySubjectNo(param);
 
 		// 전체 데이터 수
 		int totalData = courses.size();
--- Teachers_3_eleraning/src/main/java/com/ttt/dao/CourseDao.java
+++ Teachers_3_eleraning/src/main/java/com/ttt/dao/CourseDao.java
@@ -1,5 +1,22 @@
 package com.ttt.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.ttt.dto.Course3;
+
 public class CourseDao {
+	public List<Course3> selectCourseBySubjectNo(SqlSession session, Map<String, Integer> param) {
+		int cPage = param.get("cpage");
+		int numPerPage = param.get("numPerPage");
+		int subjectNo = param.get("subjectNo");
+		
+		List<Course3> result = session.selectList("course.selectCourseBySubjectNo",
+				Map.of("subjectNo", subjectNo, "start", (cPage-1)*numPerPage+1, "end", cPage*numPerPage));
+		
+		return result;
+	}
 
 }
