a3cb2499862e1a43c35460dda9a970aea1538f16
이민영 <ffrf1234tina@gmail.com>
2025-01-07 19:15:22 +0900

    my/수정/매핑 주소의 시작을 member로 변경

--- Teachers_3_eleraning/src/main/java/com/ttt/controller/member/teacher/TeacherMypageServlet.java
+++ Teachers_3_eleraning/src/main/java/com/ttt/controller/member/teacher/TeacherMypageServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/teacher/mypage/*")
+@WebServlet("/member/teacher/mypage/*")
 public class TeacherMypageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
