74d8698bc300b76eec444c4969fe515ca735755a
hoonee-math <kwanghoon041@gmail.com>
2025-01-08 09:49:24 +0900

    gh/생성/강의 관리를 위한 DAO,Service 추가

---
+++ Teachers_3_eleraning/src/main/java/com/ttt/dao/LectureDao.java
@@ -0,0 +1,25 @@
+package com.ttt.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.ttt.dto.Lecture3;
+import com.ttt.dto.ScheduleEvent3;
+
+public class LectureDao {
+    
+    // 강좌별 강의 목록 조회
+    
+    // 강의 등록 (트랜잭션 처리를 위해 반환값 int)
+    
+    // 강의 일정 등록 (일정 포함)
+    
+    // 강의 수정 (일정 포함)
+    
+    // 강의 일정 수정
+    
+    // 강의 삭제 (lecture_status 를 이용 1공개, 2삭제, 0비공개)
+    
+    // 강의와 연결된 일정 조회
+}
---
+++ Teachers_3_eleraning/src/main/java/com/ttt/service/LectureService.java
@@ -0,0 +1,13 @@
+package com.ttt.service;
+
+import com.ttt.dao.LectureDao;
+
+public class LectureService {
+
+    private LectureDao dao = new LectureDao();
+    
+    // 강좌별 강의 목록 조회
+    // 강의 등록 (일정 포함)
+    // 강의 수정 (일정 포함)
+    // 강의 삭제 (lecture_status 를 이용 1공개, 2삭제, 0비공개)
+}
