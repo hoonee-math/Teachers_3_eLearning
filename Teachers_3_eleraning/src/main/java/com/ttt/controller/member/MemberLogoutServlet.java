96c403f18b26b0ec08f10698eef00ca7bccf3e29
hoonee-math <kwanghoon041@gmail.com>
2025-01-08 14:50:13 +0900

    gh/수정/로그아웃시 쿠키 삭제 - 정상 작동

--- Teachers_3_eleraning/src/main/java/com/ttt/controller/member/MemberLogoutServlet.java
+++ Teachers_3_eleraning/src/main/java/com/ttt/controller/member/MemberLogoutServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,9 +18,30 @@ public class MemberLogoutServlet extends HttpServlet {
         super();
     }
 
+    // 로그아웃 기능을 통해 쿠키 정보 삭제 로직 구현
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
-		session.invalidate();
+		
+		// 로그인 유지용 쿠키 삭제
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				// saveId 라는 이름의 쿠키 찾기
+				if (c.getName().equals("saveId")) {
+					// 쿠키 값을 비우고 만료시간을 0으로 설정
+					c.setValue("");
+					c.setMaxAge(0);
+					c.setPath("/"); // 쿠키의 저장 경로도 동일하게 지정
+					response.addCookie(c);
+					break;
+				}
+			}
+		}
+
+	    // 세션 무효화는 쿠키 삭제 후에 실행
+	    if(session != null) {
+	        session.invalidate();
+	    }
 		
 		//요청 처리후 주소창의 주소가 /member/logout 인 문제를 해결하기 위해 msg.jsp 로 응답
 		String msg="로그아웃 되었습니다.";
