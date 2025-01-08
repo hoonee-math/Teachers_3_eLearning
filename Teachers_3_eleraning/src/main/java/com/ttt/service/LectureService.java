7e7bfbbb86b0bf48c6a6fda1daf3804b36b50a3e
hoonee-math <kwanghoon041@gmail.com>
2025-01-08 10:56:15 +0900

    gh/추가/맵퍼에 각 서비스별 쿼리문 추가, 개선 사항. 스케줄 정보에 강의 번호가 함게 들어가야함.

--- Teachers_3_eleraning/src/main/java/com/ttt/service/LectureService.java
+++ Teachers_3_eleraning/src/main/java/com/ttt/service/LectureService.java
@@ -12,73 +12,73 @@ import com.ttt.dto.ScheduleEvent3;
 
 public class LectureService {
 
-    private LectureDao dao = new LectureDao();
-    
-    // 강좌별 강의 목록 조회
-    public List<Lecture3> selectLecturesByCourseNo(int courseNo) {
-        SqlSession session = getSession();
-        List<Lecture3> lectures = dao.selectLecturesByCourseNo(session, courseNo);
-        session.close();
-        return lectures;
-    }
-    
-    // 강의 등록 (일정 포함)
-    public int insertLectureWithSchedule(Lecture3 lecture, ScheduleEvent3 event) {
-        SqlSession session = getSession();
-        int result = 0;
-        try {
-            result = dao.insertLecture(session, lecture);
-            if(result > 0 && event != null) {
-                event.setLectureNo(lecture.getLectureNo()); // 생성된 lectureNo 설정
-                result = dao.insertScheduleEvent(session, event);
-                if(result > 0) {
-                    session.commit();
-                } else {
-                    session.rollback();
-                }
-            }
-        } catch(Exception e) {
-            session.rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
-        return result;
-    }
-    
-    // 강의 수정 (일정 포함)
-    public int updateLectureWithSchedule(Lecture3 lecture, ScheduleEvent3 event) {
-        SqlSession session = getSession();
-        int result = 0;
-        try {
-            result = dao.updateLecture(session, lecture);
-            if(result > 0 && event != null) {
-                result = dao.updateScheduleEvent(session, event);
-                if(result > 0) {
-                    session.commit();
-                } else {
-                    session.rollback();
-                }
-            }
-        } catch(Exception e) {
-            session.rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
-        return result;
-    }
-    
-    // 강의 삭제 (lecture_status 를 이용 1공개, 2삭제, 0비공개)
-    public int updateDeleteLecture(int lectureNo) {
-        SqlSession session = getSession();
-        int result = dao.updateDeleteLecture(session, lectureNo);
-        if(result > 0) {
-            session.commit();
-        } else {
-            session.rollback();
-        }
-        session.close();
-        return result;
-    }
+	private LectureDao dao = new LectureDao();
+	
+	// 강좌별 강의 목록 조회
+	public List<Lecture3> selectLecturesByCourseNo(int courseNo) {
+		SqlSession session = getSession();
+		List<Lecture3> lectures = dao.selectLecturesByCourseNo(session, courseNo);
+		session.close();
+		return lectures;
+	}
+	
+	// 강의 등록 (일정 포함)
+	public int insertLectureWithSchedule(Lecture3 lecture, ScheduleEvent3 event) {
+		SqlSession session = getSession();
+		int result = 0;
+		try {
+			result = dao.insertLecture(session, lecture);
+			if(result > 0 && event != null) {
+				event.setLectureNo(lecture.getLectureNo()); // 생성된 lectureNo 설정
+				result = dao.insertScheduleEvent(session, event);
+				if(result > 0) {
+					session.commit();
+				} else {
+					session.rollback();
+				}
+			}
+		} catch(Exception e) {
+			session.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+	
+	// 강의 수정 (일정 포함)
+	public int updateLectureWithSchedule(Lecture3 lecture, ScheduleEvent3 event) {
+		SqlSession session = getSession();
+		int result = 0;
+		try {
+			result = dao.updateLecture(session, lecture);
+			if(result > 0 && event != null) {
+				result = dao.updateScheduleEvent(session, event);
+				if(result > 0) {
+					session.commit();
+				} else {
+					session.rollback();
+				}
+			}
+		} catch(Exception e) {
+			session.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+	
+	// 강의 삭제 (lecture_status 를 이용 1공개, 2삭제, 0비공개)
+	public int updateDeleteLecture(int lectureNo) {
+		SqlSession session = getSession();
+		int result = dao.updateDeleteLecture(session, lectureNo);
+		if(result > 0) {
+			session.commit();
+		} else {
+			session.rollback();
+		}
+		session.close();
+		return result;
+	}
 }
